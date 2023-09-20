b8aa29120fbed0318ac6845b33fadb71136933c1
Kishore kumar <kishorekumarsaran2001@gmail.com>
2023-09-20 13:26:35 +0530

    Dutycycle and Frequency function added

--- pwmnew.py
+++ pwmnew.py
@@ -5,7 +5,27 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
 p = GPIO.PWM(channel, 0.5)
-p.start(50)
-input('Enter a key to quit: ')
+p.start(100)
+i = None
+try:
+    while i != 'q':
+        try:
+            d = input('Set a duty cycle: ')
+            f = input('Set a frequency: ')
+
+            if(d=='q' or f=='q'):
+                break
+            
+            p.ChangeDutyCycle(int(d))
+            p.ChangeFrequency(int(f))
+
+        except ValueError as e:
+            pass
+
+except KeyboardInterrupt as e:
+    print('Received Ctrl+c ->Quitting...')
+    pass
+
 p.stop()
 GPIO.cleanup()
+print('I am exiting')
